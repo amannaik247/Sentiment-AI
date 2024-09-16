0e72525e7b3cbda4c4e099e6504f41a0137977c7
amannaik247 <amancodes247@gmail.com>
2024-09-17 02:39:12 +0530

    modified:   crewgooglegemini/tools.py

--- crewgooglegemini/tools.py
+++ crewgooglegemini/tools.py
@@ -1,9 +1,7 @@
-
 from dotenv import load_dotenv
 load_dotenv()
 import os
 
-"""
 os.environ['SERPER_API_KEY'] = os.getenv('SERPER_API_KEY')
 
 
@@ -11,8 +9,3 @@ from crewai_tools import SerperDevTool
 
 # Initialize the tool for internet searching capabilities
 tool = SerperDevTool()
-"""
-from crewai_tools import WebsiteSearchTool
-
-# Example of initiating tool that agents can use to search across any discovered websites
-tool = WebsiteSearchTool()
