448d91b83a2eff1ad5da1478e05f3878be680098
amannaik247 <amancodes247@gmail.com>
2024-12-04 02:19:57 +0530

    modified:   crewgooglegemini/crew.py 	modified:   requirements.txt

--- crewgooglegemini/crew.py
+++ crewgooglegemini/crew.py
@@ -8,17 +8,19 @@ st.title("Sentiment AI")
 
 # Get user input for topic and number of articles
 topic = st.text_input("Enter the topic")
-noofarticles = st.number_input("Enter the number of articles", min_value=4, step=1, max_value=10)
+noofarticles = st.number_input(
+    "Enter the number of articles", min_value=4, step=1, max_value=10
+)
 if st.button("Start Web Analysis"):
     # Forming the tech-focused crew with enhanced configuration
     crew = Crew(
         agents=[news_researcher, news_analyzer],
         tasks=[research_task, write_task],
-        process=Process.sequential
+        process=Process.sequential,
     )
 
     # Starting the task execution process with enhanced feedback
-    result = crew.kickoff(inputs={'topic': topic, 'noofarticles': str(noofarticles)})
-    with open("new-blog-post.md", 'r') as file:
+    result = crew.kickoff(inputs={"topic": topic, "noofarticles": str(noofarticles)})
+    with open("new-blog-post.md", "r") as file:
         content = file.read()
     st.write(content)
