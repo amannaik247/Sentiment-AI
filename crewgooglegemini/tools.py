8254bc37e0a2191da30fdd9e6618936b8e1f46b0
amannaik247 <amancodes247@gmail.com>
2024-12-05 23:52:38 +0530

    modified:   crewgooglegemini/tools.py

--- crewgooglegemini/tools.py
+++ crewgooglegemini/tools.py
@@ -1,11 +1,12 @@
 from dotenv import load_dotenv
+
 load_dotenv()
 import os
 
-os.environ['SERPER_API_KEY'] = os.getenv('SERPER_API_KEY')
+os.environ["SERPER_API_KEY"] = os.getenv("SERPER_API_KEY")
 
 
 from crewai_tools import SerperDevTool
 
 # Initialize the tool for internet searching capabilities
-tool = SerperDevTool()
+tool = SerperDevTool(n_results=3)
